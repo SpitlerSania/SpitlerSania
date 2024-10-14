7738f5155f9a44457f1bdeaf47713f0f738a4e7d
SpitlerSania <aleksandrspitlre@gmail.com>
2024-10-14 17:59:15 +0300

    Update M №4.c

--- M №4.c
+++ M №4.c
@@ -4,15 +4,15 @@
 int main() {
 	int a, k;
 
-	printf("Введите число a: ");
+	printf("Enter a number a: ");
 	scanf("%d", &a);
 
-	printf("Введите номер бита k (начиная с 0): ");
+	printf("Enter the bit number k (starting from 0): ");
 	scanf("%d", &k);
 
 	int kth_bit = (a >> k) & 1;
 
-	printf("k-ый бит числа a равен: %d\n", kth_bit);
+	printf("The kth bit of the number a is equal to: %d\n", kth_bit);
 
 	return 0;
 }
