c64d67a728596bf9f659db2ffccca81ca9cb9f50
SpitlerSania <aleksandrspitlre@gmail.com>
2024-10-28 17:54:20 +0300

    Add files via upload

---
+++ folder/M №2.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#pragma warning (disable: 4996)
+
+int main() {
+    int a, k;
+
+    printf("Enter the number a: ");
+    scanf("%d", &a);
+
+    printf("Enter the bit number k: ");
+    scanf("%d", &k);
+
+    int k_bit = (a >> k) & 1;
+
+    printf("%d-th bit number %d: %d\n", k, a, k_bit);
+
+    return 0;
+}
