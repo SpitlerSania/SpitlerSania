40c399973ec062c3f3685dc25fe7f60a2ea5cfcc
SpitlerSania <aleksandrspitlre@gmail.com>
2024-10-29 13:46:01 +0300

    Add files via upload

---
+++ Задания к работе 1 по прикладному программированию/И №3.c
@@ -0,0 +1,27 @@
+﻿/*Реализовать функцию, меняющую местами значения двух чисел типа int.Входными
+параметрами функции являются указатели типа int* .на с и*/
+#include <stdio.h>
+#pragma warning (disable: 4996)
+
+void swap(int* a, int* b) {
+    int temp = *a; // Сохраняем значение a во временной переменной temp
+    *a = *b;       // Меняем значение a на значение b
+    *b = temp;     // Присваиваем temp значение a
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Введите первое число: "); //a
+    scanf("%d", &num1);
+
+    printf("Введите второе число: "); //b
+    scanf("%d", &num2);
+
+    printf("До обмена числами: num1 = %d, num2 = %d\n", num1, num2);
+
+    swap(&num1, &num2);
+    printf("После обмена числами: num1 = %d, num2 = %d\n", num1, num2);
+
+    return 0;
+}
---
+++ Задания к работе 1 по прикладному программированию/И №7.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#pragma warning (disable: 4996)
+
+#define SIZE 10 // Размер исходного массива
+
+int main() {
+    int array[SIZE], odd_index_array[SIZE / 2 + 1]; // Новый массив
+    int new_size = 0; // Размер нового массива
+
+    printf("Введите %d чисел:\n", SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    for (int i = 1; i < SIZE; i += 2) {
+        odd_index_array[new_size++] = array[i]; // Записываем в новый массив
+    }
+
+    // Вывод нового массива
+    printf("Числа на нечётных позициях:\n");
+    for (int i = 0; i < new_size; i++) {
+        printf("%d ", odd_index_array[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
