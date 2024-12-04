cb959a3b4da432c77f14552936db6e465a6fa9cd
SpitlerSania <aleksandrspitlre@gmail.com>
2024-12-04 20:39:13 +0300

    Add files via upload

---
+++ Задания к работе 1 по прикладному программированию/И №13.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+
+
+int main(void) {
+	char str[] = "i wrote almost the entire program myself, getting smarter";
+	char* ptr = str;
+
+	for (int i = 0; *ptr != '\0'; ++i, ++ptr) {
+		if (i % 2 == 1) {
+			*ptr = (char)toupper((unsigned char)*ptr);
+		}
+	}
+
+	setlocale(LC_ALL, "Russian");
+
+	printf("Результат: %s", str);
+
+	return 0;
+}
