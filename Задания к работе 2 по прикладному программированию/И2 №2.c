7b2ff78becfa430804d5091cf945c02f247bed85
SpitlerSania <aleksandrspitlre@gmail.com>
2024-12-17 17:01:05 +0300

    И2 №2.c

--- Задания к работе 2 по прикладному программированию/И2 №2.c
+++ Задания к работе 2 по прикладному программированию/И2 №2.c
@@ -1,7 +1,6 @@
-﻿#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-#include <time.h>
-#include <locale.h>
 #pragma warning(disable: 4996)
 
 size_t my_strlen(const char* str) {
@@ -9,10 +8,12 @@ size_t my_strlen(const char* str) {
     while (*str++) {
         length++;
     }
+
     return length;
 }
 
-int my_strcmp(const char* s1, const char* s2) {
+int my_strcmp(const char* s1, const char* s2) { //-l
+
     while (*s1 && *s2 && *s1 == *s2) {
         s1++;
         s2++;
@@ -22,6 +23,7 @@ int my_strcmp(const char* s1, const char* s2) {
 }
 
 void my_strcat(char* first_word, char* second_word) {
+
     char* ptr = first_word;
     while (*ptr != '\0') {
         ptr++;
@@ -30,7 +32,8 @@ void my_strcat(char* first_word, char* second_word) {
     while ((*ptr++ = *second_word++) != '\0');
 }
 
-void reverse_string(char* str) {
+void reverse_string(char* str) { //-r
+
     size_t len = my_strlen(str);
     size_t i, j;
 
@@ -53,7 +56,7 @@ char* my_strcpy(char* dest, const char* src) {
     return ptr;
 }
 
-void converting_element(char* str) {
+void converting_element(char* str) { //-u
     if (str == NULL || my_strlen(str) == 0) {
         return;
     }
@@ -63,11 +66,20 @@ void converting_element(char* str) {
     }
 }
 
-void rearrange(char* str) {
+void rearrange(char* str) { //-n
+
+    if (str == NULL) {
+        fprintf(stderr, "Ошибка входной строки\n");
+        return;
+    }
+
     char digits[BUFSIZ] = { 0 };
     char letters[BUFSIZ] = { 0 };
     char others[BUFSIZ] = { 0 };
-    char* digits_ptr = digits, *letters_ptr = letters, *others_ptr = others, *str_ptr = str;
+    char* digits_ptr = digits;
+    char* letters_ptr = letters;
+    char* others_ptr = others;
+    char* str_ptr = str;
 
     while (*str_ptr++) {
         if (isdigit(*str_ptr)) {
@@ -75,10 +87,41 @@ void rearrange(char* str) {
         }
         else if (isalpha(*str_ptr)) {
             *letters_ptr++ = *str_ptr;
-        } 
+        }
         else {
             *others_ptr++ = *str_ptr;
         }
+    }
+
+    while (*str_ptr) {
+        if (isdigit(*str_ptr)) {
+            if (digits_ptr - digits < BUFSIZ - 1) {
+                *digits_ptr++ = *str_ptr;
+            }
+            else {
+                fprintf(stderr, "Buffer overflow for digits\n");
+                return;
+            }
+        }
+        else if (isalpha(*str_ptr)) {
+            if (letters_ptr - letters < BUFSIZ - 1) {
+                *letters_ptr++ = *str_ptr;
+            }
+            else {
+                fprintf(stderr, "Buffer overflow for letters\n");
+                return;
+            }
+        }
+        else {
+            if (others_ptr - others < BUFSIZ - 1) {
+                *others_ptr++ = *str_ptr;
+            }
+            else {
+                fprintf(stderr, "Buffer overflow for other characters\n");
+                return;
+            }
+        }
+        str_ptr++;
     }
 
     *digits_ptr = '\0';
@@ -88,6 +131,7 @@ void rearrange(char* str) {
     my_strcpy(str, digits);
     my_strcat(str, letters);
     my_strcat(str, others);
+
 }
 
 int main(int argc, char* argv[]) {
@@ -101,20 +145,20 @@ int main(int argc, char* argv[]) {
 
     if (my_strcmp(flag, "-l") == 0) {
         printf("Length of the string: %zu\n", my_strlen(str));
-    } 
+    }
     else if (my_strcmp(flag, "-r") == 0) {
         reverse_string(str);
         printf("Reversed string: %s\n", str);
-    } 
+    }
     else if (my_strcmp(flag, "-u") == 0) {
         converting_element(str);
         printf("String with alternating uppercase: %s\n", str);
-    } 
+    }
     else if (my_strcmp(flag, "-n") == 0) {
         rearrange(str);
         printf("Rearranged string: %s\n", str);
 
-    } 
+    }
     else if (my_strcmp(flag, "-c") == 0) {
         if (argc < 4) {
             printf("Usage: %s -c <seed> <strings...>\n", argv[0]);
@@ -124,8 +168,8 @@ int main(int argc, char* argv[]) {
             my_strcat(str, argv[i]);
         }
         printf("Concatenated string: %s\n", str);
-    } 
-        else {
+    }
+    else {
         printf("Unknown flag: %s\n", flag);
         return 1;
     }
