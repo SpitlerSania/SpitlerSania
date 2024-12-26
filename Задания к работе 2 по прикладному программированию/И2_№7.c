c37357b915d1df806112d164509630a28397f1a7
SpitlerSania <aleksandrspitlre@gmail.com>
2024-12-26 19:52:52 +0300

    Add files via upload

---
+++ Задания к работе 2 по прикладному программированию/И2_№7.c
@@ -0,0 +1,100 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <locale.h>
+#pragma warning (disable: 4996)
+
+
+int search_for_positive_integers(const char* str, int base) {
+    size_t i = 0;
+    int result = 0;
+    for (size_t i; str[i]; ++i) {
+        if ('0' <= str[i] && str[i] <= '9') {
+            result = result * base + (str[i] - '0');
+            //123
+            //На первом шаге : result = 0 * 10 + ('1' - '0') = 1
+            //На втором шаге : result = 1 * 10 + ('2' - '0') = 12
+            //На третьем шаге : result = 12 * 10 + ('3' - '0') = 123
+        }
+        else if ('a' <= str[i] && str[i] <= 'z') {
+            result = result * base + (str[i] - 'a' + 10);
+            //1ab
+            //На первом шаге: result = 0 * 16 + ('1' - '0') = 1
+            //На втором шаге : result = 1 * 16 + ('a' - 'a' + 10) = 16 + 10 = 26
+            //На третьем шаге : result = 26 * 16 + ('b' - 'a' + 10) = 416 + 11 = 427
+        }
+
+
+        else if ('A' <= str[i] && str[i] <= 'Z') {
+            result = result * base + (str[i] - 'A' + 10);
+        }
+    }
+    return 0;
+}
+
+
+int checking_number_kaprekar(int num, int base) {
+    long square_num = (long)num * num; //квадрат числа
+
+    char buffer[252];
+    sprintf(buffer, "%ld", square_num);
+
+    size_t len = strlen(buffer);
+    size_t split_index = len / 2;
+
+    char left_part[50], right_part[50];
+
+    memset(left_part, 0, sizeof(left_part)); //memset обнуляет буферы
+    memset(right_part, 0, sizeof(right_part));
+    strncpy(left_part, buffer, split_index);// Прототипchar: *strncpy (char *dst, const char *src, size_t len);
+    strcpy(right_part, &buffer[split_index]);
+
+    int left_num = search_for_positive_integers(left_part, base); // Преобразование левой части
+    int right_num = search_for_positive_integers(right_part, base); // Преобразование правой части
+
+    if (left_num == 0 || right_num == 0) {
+        return 0;
+    }
+    else {
+        return (left_num + right_num == num);
+    }
+}
+
+void clear_memory() {
+    char* ptr = NULL;
+    size_t size = 0;
+    while (ptr != NULL && size < sizeof(ptr)) {
+        *(ptr + size) = 0;
+        ptr++;
+        size += sizeof(*ptr);
+    }
+    free(ptr);
+}
+
+void search_kaprekars(int base, const char** strings, size_t count) { //count количество сто=рок в массиве
+    printf("Основание системы исчисления: %d \n", base);
+    printf("Числа Капрекера: \n");
+
+    for (size_t i = 0; i < count; ++i) {
+        int num = search_for_positive_integers(strings[i], base);
+
+        if (checking_number_kaprekar(num, base)) {
+            printf("%s ", strings[i]);
+        }
+    }
+
+    printf("\n");
+
+    void clear_memory();
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    const char* strings[] = { "2234", "5869", "5542", "5525", "9", "45", "297", "100", "147", "10110", "121" };
+    size_t count = sizeof(strings) / sizeof(strings[0]);
+
+    search_kaprekars(10, strings, count);
+
+    return 0;
+}
---
+++ Задания к работе 2 по прикладному программированию/И2_№8.c
@@ -0,0 +1,101 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void reverse_string(char* str) {
+    size_t len = strlen(str);
+    for (size_t i = 0; i < len / 2; ++i) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
+
+char* column_addition(char* first, char* second, int base) {
+
+    size_t max_len = strlen(first) > strlen(second) ? strlen(first) + 1 : strlen(second) + 1;
+
+    char* result = malloc(max_len + 2);
+    if (result == NULL) {
+        fprintf(stderr, "Ошибка выделения памяти");
+        return NULL;
+    }
+
+    reverse_string(first);
+    reverse_string(second);
+
+    int temp = 0;
+    for (size_t i = 0; i < max_len; ++i) {
+
+        int digit_first;
+        if (i < strlen(first)) {
+            digit_first = first[i] - '0';
+        }
+        else {
+            digit_first = 0;
+        }
+
+        int digit_second;
+        if (i < strlen(second)) {
+            digit_second = second[i] - '0';
+        }
+        else {
+            digit_second = 0;
+        }
+
+        int sum = digit_first + digit_second + temp;
+        temp = sum / base;
+        result[i] = (sum % base) + '0';
+    }
+
+    if (temp != 0) {
+        result[max_len] = temp + '0';
+    }
+
+    result[max_len + 1] = '\0';
+
+    // Переворачиваем строку обратно
+    reverse_string(result);
+
+    return result;
+}
+
+char* sum_numbers(int count, char** numbers, int base) {
+    if (count == 0) {
+        return NULL;
+    }
+
+    char* result = numbers[0];
+
+    for (int i = 1; i < count; ++i) {
+        char* temp = column_addition(result, numbers[i], base);
+        free(result);
+        result = temp;
+    }
+
+    while (*result == '0' && *result != '\0') {
+        memmove(result, result + 1, strlen(result));
+    }
+
+    return result;
+}
+
+int main() {
+
+    int base = 16;  // Основание системы счисления
+    int n = 4;      // Количество чисел
+    char* numbers[] = { "C", "15", "35", "ME" };  // Шестнадцатиричная система
+
+    char* result = sum_numbers(n, numbers, base);
+
+    if (result) {
+        printf("Сумма чисел в системе счисления с основанием %d равна %s.\n", base, result);
+        free(result);
+    }
+    else {
+        printf("Error.\n");
+    }
+
+    return 0;
+}
