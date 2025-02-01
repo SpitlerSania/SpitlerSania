c72614e87c67b0514daf07998842756a459009fa
SpitlerSania <aleksandrspitlre@gmail.com>
2025-02-01 14:47:44 +0300

    Update И №1.c

--- Задания к работе 1 по прикладному программированию/И №1.c
+++ Задания к работе 1 по прикладному программированию/И №1.c
@@ -1,21 +1,45 @@
-﻿#include<stdio.h>
-// Главная функция
-int main() {
-	int array[5] = { 5,4,2,1,3 };
+#include <stdio.h>
+#define EMPTY_ARRAY 1
+
+int Bouble_Sort(int arr[], int n) {
+
+	if (arr == NULL) {
+		printf("Error");
+		return EMPTY_ARRAY;
+	}
 	int i, j, temp;
 
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4 - i; j++) {
-			if (array[j] > array[j + 1]) { //Сравневаем
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 			}
 		}
 	}
+}
 
-	for (i = 0; i < 5; i++) {
-		printf("%d ", array[i]);//Выводим
+void Arr_Printf(int arr[], int size) {
+	int i;
+
+	for (i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
 	}
+}
+
+int main() {
+
+	int arr[] = {1, 3, 5, 2, 4, 6, 9, 7, 8, 10};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	printf("Не отсортированный массив: ");
+	Arr_Printf(arr, n);
+
+	Bouble_Sort(arr, n);
+	printf("\nОтсортированный массив: ");
+
+	Arr_Printf(arr, n);
+
 	return 0;
 }
